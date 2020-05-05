cb607d8b563400e821cca080c87d891c177577f8
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2020-05-05 14:04:48 +0100

    Move script functions to testable module, add tests
    Move script helpers to subfolder
    
    Dockerfile update

--- scripts/render-template.py
+++ scripts/render-template.py
@@ -1,71 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import sys
+sys.path.insert(0, '.')
 
-from six.moves.urllib.parse import urljoin
-
-import jinja2
-from jinja2.runtime import StrictUndefined
-
-
-def load_file(path):
-    with open(path) as f:
-        return f.read()
-
-
-def _urljoin(base, url):
-    """
-    jinja filter to allow templates to perform a correct "url join". can also be used to conditionally add a trailing
-    slash to base if not already present - particularly useful when passing an url to the proxy_pass directive which
-    behaves differently depending on whether a target address has an url "path" part
-    """
-    return urljoin(base, url, allow_fragments=False)
-
-
-def template_string(string, variables, templates_path):
-    jinja_env = jinja2.Environment(
-        autoescape=False,
-        # the jinja templates have no user input and are used to generate config files
-        # we are not confident that autoescaping will be useful here and we have therefore taken a positive decision
-        # to turn it off
-        trim_blocks=True,
-        undefined=StrictUndefined,
-        loader=jinja2.FileSystemLoader(templates_path)
-    )
-
-    jinja_env.filters["urljoin"] = _urljoin
-
-    try:
-        template = jinja_env.from_string(string)
-    except jinja2.exceptions.TemplateSyntaxError as e:
-        raise ValueError(u"Template error: {}".format(e))
-
-    try:
-        return template.render(variables)
-    except jinja2.exceptions.UndefinedError as e:
-        raise ValueError(u"Variable {} in '{}'".format(e, string))
-
-
-def main(template_name):
-    variables = {
-        key.replace('DM_', '').lower(): value
-        for key, value in os.environ.items()
-        if key.startswith('DM_')
-    }
-
-    variables.update({
-        'static_files_root': '/usr/share/nginx/html'
-    })
-
-    with open(template_name, 'r') as template_file:
-        return template_string(
-            template_file.read(),
-            variables=variables,
-            templates_path=os.path.dirname(template_name)
-        )
-
+from script_helpers.render_template_helpers import render_nginx_template
 
 if __name__ == "__main__":
-    print(main(sys.argv[1]))
+    print(render_nginx_template(sys.argv[1]))
---
+++ scripts/script_helpers/render_template_helpers.py
@@ -0,0 +1,62 @@
+import os
+from six.moves.urllib.parse import urljoin
+
+import jinja2
+from jinja2.runtime import StrictUndefined
+
+
+def load_file(path):
+    with open(path) as f:
+        return f.read()
+
+
+def _urljoin(base, url):
+    """
+    jinja filter to allow templates to perform a correct "url join". can also be used to conditionally add a trailing
+    slash to base if not already present - particularly useful when passing an url to the proxy_pass directive which
+    behaves differently depending on whether a target address has an url "path" part
+    """
+    return urljoin(base, url, allow_fragments=False)
+
+
+def template_string(string, variables, templates_path):
+    jinja_env = jinja2.Environment(
+        autoescape=False,
+        # the jinja templates have no user input and are used to generate config files
+        # we are not confident that autoescaping will be useful here and we have therefore taken a positive decision
+        # to turn it off
+        trim_blocks=True,
+        undefined=StrictUndefined,
+        loader=jinja2.FileSystemLoader(templates_path)
+    )
+
+    jinja_env.filters["urljoin"] = _urljoin
+
+    try:
+        template = jinja_env.from_string(string)
+    except jinja2.exceptions.TemplateSyntaxError as e:
+        raise ValueError(u"Template error: {}".format(e))
+
+    try:
+        return template.render(variables)
+    except jinja2.exceptions.UndefinedError as e:
+        raise ValueError(u"Variable {} in '{}'".format(e, string))
+
+
+def render_nginx_template(template_path):
+    variables = {
+        key.replace('DM_', '').lower(): value
+        for key, value in os.environ.items()
+        if key.startswith('DM_')
+    }
+
+    variables.update({
+        'static_files_root': '/usr/share/nginx/html'
+    })
+
+    with open(template_path, 'r') as template_file:
+        return template_string(
+            template_file.read(),
+            variables=variables,
+            templates_path=os.path.dirname(template_path)
+        )
